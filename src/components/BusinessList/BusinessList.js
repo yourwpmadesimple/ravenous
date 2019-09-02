b76a3b1e0bd1971549ea8580f0831de1f88542e2
yourwpmadesimple <wayne@yourwpmadesimple.com>
2019-09-02 10:21:48 -0500

    Createe .map for BusinessList

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -6,11 +6,9 @@ class BusinessLIst extends React.Component{
     render(){
         return (
         <div className="BusinessList">
-            <Business />
-            <Business />
-            <Business />
-            <Business />
-            <Business />
+            {this.props.businesses.map((business, i) =>
+                <Business key={i} business={business}/>
+            )}
         </div>
         );
     }
